8d1cae0e981696cf485b102ae0e926f687b3aed3
Facundo Sad <fsad@buenosaires.gob.ar>
2023-10-14 14:18:58 -0300

    adding pagination

--- app/Http/Livewire/UsersTable.php
+++ app/Http/Livewire/UsersTable.php
@@ -4,13 +4,16 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\User;
+use Livewire\WithPagination;
 
 class UsersTable extends Component
 {
+    use WithPagination;
+
     public function render()
     {
         return view('livewire.users-table',[
-            'users' => User::all()
+            'users' => User::paginate(10)
         ]);
     }
 }
--- resources/views/livewire/users-table.blade.php
+++ resources/views/livewire/users-table.blade.php
@@ -53,8 +53,8 @@
                                     class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     {{$user->name}}</th>
                                 <td class="px-4 py-3">{{$user->email}}</td>
-                                <td class="px-4 py-3 text-green-500">
-                                    admin</td>
+                                <td class="px-4 py-3 text-{{$user->is_admin ? "green-500" : "blue-500"}}">
+                                    {{$user->is_admin ? 'Admin' : 'User'}}</td>
                                 <td class="px-4 py-3">{{$user->created_at}}</td>
                                 <td class="px-4 py-3">{{$user->updated_at}}</td>
                                 <td class="px-4 py-3 flex items-center justify-end">
@@ -70,18 +70,8 @@
 
                 <div class="py-4 px-3">
                     <div class="flex ">
-                        <div class="flex space-x-4 items-center mb-3">
-                            <label class="w-32 text-sm font-medium text-gray-900">Per Page</label>
-                            <select
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
-                                <option value="5">5</option>
-                                <option value="10">10</option>
-                                <option value="20">20</option>
-                                <option value="50">50</option>
-                                <option value="100">100</option>
-                            </select>
-                        </div>
                     </div>
+                    {{$users->links()}}
                 </div>
             </div>
         </div>
